d46babbc43562f5a47dd5c8ce47b0746c352cab0
AVVS <v@aminev.me>
2015-11-21 14:57:40 +0300

    Remove extra logger creation from binary

--- bin/mservice.js
+++ bin/mservice.js
@@ -3,7 +3,6 @@
 // accepts conf through .env file
 // suitable for configuring this in the docker env
 var configuration = require('ms-amqp-conf');
-var bunyan = require('bunyan');
 
 var dir;
 if (process.env.NODE_ENV === 'production') {
@@ -13,38 +12,14 @@ if (process.env.NODE_ENV === 'production') {
   require('../test/babelhook.js');
 }
 
-var streams = [{
-  level: 'trace',
-  type: 'raw',
-  stream: new bunyan.RingBuffer({ limit: 100 }),
-}];
-if (configuration.logger) {
-  streams.push({
-    stream: process.stdout,
-    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-  });
-}
-
 var Service = require(dir);
 var service = new Service(configuration);
-var logger = bunyan.createLogger({
-  name: service._config.name || 'ms-mailer',
-  streams: streams,
-});
-service.on('log', function writeLogs(namespace, message) {
-  if (message) {
-    logger.info({ namespace }, message);
-  } else {
-    logger.info(namespace);
-  }
-});
-
 service.connect()
   .then(function serviceStarted() {
-    logger.info('service started');
+    service.log.info('service started');
   })
   .catch(function serviceCrashed(err) {
-    logger.fatal('service crashed', err);
+    service.log.fatal('service crashed', err);
     setImmediate(function escapeTryCatch() {
       throw err;
     });
