8cb50cb182e8ee840ef2852ba4e67b85290a44dd
AVVS <v@aminev.me>
2016-01-13 01:32:52 +0300

    fix: actions.retry conditions are more strict

--- src/mailer.js
+++ src/mailer.js
@@ -12,19 +12,20 @@ const path = require('path');
  * Accepts/rejects messages
  */
 function ack(err, data, actionName, actions) {
-  if (err) {
-    if (err.name === 'ValidationError') {
-      actions.reject();
-      return Promise.reject(err);
-    }
+  if (!err) {
+    actions.ack();
+    return data;
+  }
 
-    this.log.error('error performing operation %s. Scheduling retry', actionName, err);
-    actions.retry();
+  if (err.name === 'ValidationError' || actionName === 'adhoc') {
+    actions.reject();
     return Promise.reject(err);
   }
 
-  actions.ack();
-  return data;
+  // assume that predefined accounts must not fail - credentials are correct
+  this.log.error('error performing operation %s. Scheduling retry', actionName, err);
+  actions.retry();
+  return Promise.reject(err);
 }
 
 /**
