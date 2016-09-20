8e400c434b045c052ab090f53f0f3d6fb388a80a
AVVS <v@makeomatic.ru>
2016-09-20 11:40:45 +0300

    fix: style & switch-case

--- src/actions/predefined.js
+++ src/actions/predefined.js
@@ -1,3 +1,5 @@
+const sendMail = require('../utils/sendMail');
+
 /**
  * Sends message via a predefined account
  * @param  {Mixed} params { account: String, email, [ctx] }
@@ -6,5 +8,5 @@
 module.exports = function predefined({ params }) {
   return this
     .getTransport(params.account)
-    .then(transport => this.sendMail(transport, params.email, params.ctx));
+    .then(transport => sendMail(transport, params.email, params.ctx));
 };
